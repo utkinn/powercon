82072d1bc256f4d6115689da2a12187baeb093fb
Nikita <shockck84@gmail.com>
2018-06-30 18:39:41 +0500

    Move PCSubmitButton's color to a constant

--- lua/client/powercon_submit_button.lua
+++ lua/client/powercon_submit_button.lua
@@ -1,3 +1,4 @@
+local BUTTON_COLOR = Color(63, 201, 36, 200)  -- Green
 local submit = {}
 
 function submit:Init()
@@ -5,7 +6,8 @@ function submit:Init()
 end
 
 function submit:Paint(width, height)
-    draw.RoundedBox(4, 0, 0, width, height, Color(63, 201, 36, 200))
+    draw.RoundedBox(4, 0, 0, width, height, BUTTON_COLOR)
+    -- TODO: Add ">"
 end
 
 vgui.Register('PCSubmitButton', submit, 'DButton')

b43d9f40ec6fc572d61602ca2187bb6b723b9cd6
Nikita <shockck84@gmail.com>
2018-07-01 13:45:03 +0500

    Improve text area

--- lua/client/powercon_text_area.lua
+++ lua/client/powercon_text_area.lua
@@ -2,12 +2,21 @@ local textarea = {}
 
 function textarea:Init()
     self:DockMargin(5,5,5,5)
+    self.textPaddingX = 1
+    self.textPaddingY = 1
 end
 
 function textarea:Paint(width, height)
-    draw.RoundedBox(4, 0, 0, width, height, Color(0, 0, 0, 200))
+    if self:HasFocus() then
+        surface.SetDrawColor(255, 255, 255, 200)
+        surface.DrawOutlinedRect(0, 0, width, height)
+    end
+    --     draw.RoundedBox(4, 0, 0, width, height, Color(20, 20, 20, 200))
+    -- else
+        draw.RoundedBox(4, 0, 0, width, height, Color(0, 0, 0, 200))
+    -- end
 
-    draw.DrawText(self:GetValue(), 'UbuntuMono', 1, 1, Color(255, 255, 255, 220))
+    draw.DrawText(self:GetValue(), 'UbuntuMono18', self.textPaddingX, self.textPaddingY, Color(255, 255, 255, 220))
     -- TODO: Check out Markup funcs
 end
 
